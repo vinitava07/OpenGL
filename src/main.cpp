29c153157193a961a7c04cc68657f70e34ade946
vinitava07 <vinitava07@gmail.com>
2024-11-07 23:30:17 -0300

    casinha

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,11 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <Windows.h>
+#include <cstdio>
 
-//const char* vertexShaderSource = "#version 330 core\n"
-//"layout (location = 0) in vec3 aPos;\n"
-//"void main()\n"
-//"{\n"
-//"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-//"}\0";
-//const char* fragmentShaderSource = "#version 330 core\n"
-//"out vec4 FragColor;\n"
-//"void main()\n"
-//"{\n"
-//"	FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
-//"}\n\0";
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
 
 static GLuint CompileShader(GLuint type, const std::string& source) {
 
@@ -22,7 +14,7 @@ static GLuint CompileShader(GLuint type, const std::string& source) {
 	glShaderSource(id, 1, &src, NULL);
 	glCompileShader(id);
 
-	//TODO: Error handling
+	//Error handling
 	GLint result;
 	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
 	if (result == GL_FALSE)
@@ -34,6 +26,7 @@ static GLuint CompileShader(GLuint type, const std::string& source) {
 		std::cout << message << std::endl;
 
 	}
+	//Error handling
 
 	return id;
 }
@@ -57,12 +50,30 @@ static GLuint CreateShader(const std::string& vertexShader, const std::string& f
 
 int main(void)
 {
+	SetConsoleOutputCP(GetACP());
+	std::string vertexShader =
+		"#version 330 core\n"
+		"layout (location = 0) in vec3 aPos;\n"
+		"void main()\n"
+		"{\n" 
+		"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+		"}\0";
+
+	std::string fragmentShader =
+		"#version 330 core\n"
+		"out vec4 FragColor;\n"
+		"void main()\n"
+		"{\n"
+		"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+		"}\n\0";
+
+
 	GLFWwindow* window;
+
 	// DIRETIVAS PARA INICIALIZAR A JANELA
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
 
 	/* Initialize the library */
 	if (!glfwInit()) {
@@ -90,62 +101,93 @@ int main(void)
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
-	GLfloat positions[6] = {
-		-0.5f, -0.5f,
-		 0.0f,  0.5f,
-		 0.5f, -0.5f,
+	GLfloat vertices[] = {
+		 0.5f,  0.5f, 0.0f,  // top right 0 
+		 0.5f, -0.5f, 0.0f,  // bottom right 1
+		-0.5f, -0.5f, 0.0f,  // bottom left 2 
+		-0.5f,  0.5f, 0.0f,  // top left 3 
+		 0.0f,  1.0f, 0.0f,  //screen top 4
 	};
-	GLuint buffer;
-	glGenBuffers(1, &buffer);
-	glBindBuffer(GL_ARRAY_BUFFER, buffer);
-	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(GLfloat), positions, GL_STATIC_DRAW);
 
+	GLuint indices[] = {  // note that we start from 0!
+		0, 1, 3,   // first triangle
+		1, 2, 3,   // second triangle
+		4, 0, 3
+
+	};
+
+	//VERTEX BUFFER OBJECT, VERTEX ARRAY OBJECT, ELEMENT BUFFER OBJECT
+	GLuint VBO, VAO, EBO;
+	glGenVertexArrays(1, &VAO);
+	//glBindVertexArray(VAO);
+
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 3, 0);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 2, 0);
 
-	std::string vertexShader =
-		"#version 330 core\n"
-		"\n"
-		"layout (location = 0) in vec4 position;\n"
-		"\n"
-		"void main()\n"
-		"{\n"
-		"	gl_Position = position;\n"
-		"}\n";
 
-	std::string fragmentShader =
-		"#version 330 core\n"
-		"\n"
-		"layout (location = 0) out vec4 color;\n"
-		"\n"
-		"void main()\n"
-		"{\n"
-		"	color = vec4(1.0, 0.0, 0.0, 1.0);\n"
-		"}\n";
 	GLuint shader = CreateShader(vertexShader, fragmentShader);
-	glUseProgram(shader);
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
-		///* Render here */
+
+		// input
+		// -----
+		processInput(window);
+
+		/* Render here */
 		// Clean the back buffer and assign the new color to it
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glUseProgram(shader);
+
+		//glBindVertexArray(VAO);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+		//glBindVertexArray(0);
+
 
 		// Swap the back buffer with the front buffer
 		glfwSwapBuffers(window);
-
 		/* Poll for and process events */
 		glfwPollEvents();
 	}
 
 
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
 	glDeleteProgram(shader);
 
 	glfwDestroyWindow(window);
 
 	glfwTerminate();
 	return 0;
+}
+
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// make sure the viewport matches the new window dimensions; note that width and 
+	// height will be significantly larger than specified on retina displays.
+	glViewport(0, 0, width, height);
 }
